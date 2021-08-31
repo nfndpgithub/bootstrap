d2c512f89eca7d7fab1a513f5698e210863e86f4
nfndp <nfundup@gmail.com>
2021-08-31 02:33:36 +0200

    sort radi ali jedva

--- query.php
+++ query.php
@@ -1,39 +1,41 @@
 <?php
+
 $connect=mysqli_connect('localhost','root','','korisnici');
 $output='';
-$order=$_POST("order");
+$order=$_POST["order"];
 if($order=='decs'){
     $order='asc';
 }else{
     $order='desc';
 }
 $query="select id ime,prezime, email from users order by ".$_POST["column_name"]." ".$_POST["order"]."";
-$result=$mysqli_query($connect,$query);
+$result=mysqli_query($connect,$query);
 $output.='
 
-<table class="table " id="table">
-  <thead>
+ <table class="table ">
+ <thead>
     <tr>
-      <th  ><a id="id" data-order="'.$order'" href="#">ID</a></th>
-      <th  > <a id="ime" data-order="'.$order'" href="#">Ime</a></th>
-      <th  > <a id="prezime" data-order="'.$order'" href="#">Prezime</a></th>
-      <th  > <a id="email" data-order="'.$order'" href="#">Email</a></th>
+      <th  ><a class="column_sort" id="id" data-order="'.$order.'" href="#" >ID</a></th>
+      <th  > <a class="column_sort" id="ime" data-order="'.$order.'" href="#">Ime</a></th>
+      <th  > <a class="column_sort" id="prezime" data-order="'.$order.'" href="#">Prezime</a></th>
+      <th  > <a class="column_sort" id="email" data-order="'.$order.'" href="#">Email</a></th>
     </tr>
-  </thead>
-  <tbody>
+    </thead>
+    <tbody>
+  
   ';
   while($row=mysqli_fetch_array($result)){
 
-  $output.= '
+  $output.='
     <tr>
-      <th scope="row"><?php echo $row["id"] ?></th>
-      <td><?php echo $row["ime"] ?></td>
-      <td><?php echo $row["prezime"] ?></td>
-      <td><?php echo $row["email"] ?></td>
+    <td >'.$row["id"].'</td> 
+    <td>'.$row["ime"].'</td>
+    <td>'.$row["prezime"].'</td>
+    <td>'.$row["email"].'</td>
     </tr>
   ';
   }
- $output=.'</tbody> </table>';
+ $output.=' </tbody> </table>';
  echo $output;
 
 
--- sort.php
+++ sort.php
@@ -9,12 +9,15 @@ $result= mysqli_query($connect,$query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Sort</title>
+    
 </head>
 <body>
 <!-- navbar -->
@@ -38,14 +41,14 @@ $result= mysqli_query($connect,$query);
 <!-- table -->
 <div class="container py-3  " align="center">
 <h2 class="py-3" >Prijavljeni</h2>
-<div class="table-responsive">
-<table class="table " id="table">
+<div class="table-responsive" id="table">
+<table class="table ">
   <thead>
     <tr>
-      <th  ><a id="id" data-order="desc" href="#">ID</a></th>
-      <th  > <a id="ime" data-order="desc" href="#">Ime</a></th>
-      <th  > <a id="prezime" data-order="desc" href="#">Prezime</a></th>
-      <th  > <a id="email" data-order="desc" href="#">Email</a></th>
+      <th  ><a class="column_sort" id="id" data-order="desc" href="#" >ID</a></th>
+      <th  > <a class="column_sort" id="ime" data-order="desc" href="#">Ime</a></th>
+      <th  > <a class="column_sort" id="prezime" data-order="desc" href="#">Prezime</a></th>
+      <th  > <a class="column_sort" id="email" data-order="desc" href="#">Email</a></th>
     </tr>
   </thead>
   <tbody>
@@ -54,44 +57,51 @@ $result= mysqli_query($connect,$query);
 
   ?>
     <tr>
-      <th scope="row"><?php echo $row["id"] ?></th>
+      <td ><?php echo $row["id"] ?></td>
       <td><?php echo $row["ime"] ?></td>
       <td><?php echo $row["prezime"] ?></td>
       <td><?php echo $row["email"] ?></td>
     </tr>
-  <?php }
+  <?php 
+  }
   ?>
     
   </tbody>
 </table>
 </div>
 </div>
-<script>
-<?php
-echo "kdlsajflsad";
-?>
-$(document).ready(function(){
-    $(document).on('click','.column_sort',function (){
-        var column_name=$(this).attr("id");
-        var order=$(this).data("order");
-        var arrow='';
-        if(order=='desc'){
-            arrow=<i class="bi bi-arrow-down-cicrle-fill"></i>
-        }else{
-            arrow=<i class="bi bi-arrow-up-cicrle-fill"></i>
-        }
-       $.ajax({
-           url:"query.php",
-           method:"POST",
-           data:{column_name:column_name,oder:order},
-           success:function(data){
-               $('#table').html(data);
-               $('#'+column_name+'').append(arrow);
 
-           }
-       })
-    })
-})
-</script>
 </body>
 </html>
+<script>
+
+    
+    $(document).ready(function(){
+    $(document).on('click','.column_sort',function (){
+      //alert("helloo");
+         var column_name=$(this).attr("id");
+        var order=$(this).data("order");
+        var arrow='';
+        
+        if(order=='desc'){
+            
+            arrow='&nbsp; <span class="glyphicon glyphicon-arrow-down"></span>';
+        }else{
+            arrow='&nbsp; <span class="glyphicon glyphicon-arrow-up"></span>';
+            
+        }
+        $.ajax({
+           url:"query.php",
+           method:"POST",
+           data:{column_name:column_name,order:order},
+           success:function(data){
+             
+                $('#table').html(data);
+               //$('#'+column_name+'').append(arrow); 
+ 
+           }
+       }) 
+    });
+});
+
+</script>
